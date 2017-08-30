699f6bf94d6d952234e48ef8ea47e4c3a3cc704a
manoj8412 <manoj8412@gmail.com>
2017-08-30 19:36:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name = 'my-first-package-script',         # This is the name of your PyPI-package.
     version = '0.1',                          # Update the version number for new releases
-    scripts = ['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts = ['name']                  # The name of your scipt, and also the command you'll be using for calling it
 )
